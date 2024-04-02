2157c962b9a48418f5ff8cae5d4e0b29f6e14a96
worldbeater2 <realdudization@gmail.com>
2024-04-02 01:10:43 +0000

    Api fix 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-
 import useFetch from "./hooks/useFetch";
 import { BlogContent } from "./pages";
 import Homepage from "./pages/Homepage";
 import { Routes, Route } from "react-router-dom";
 
-
 function App() {
+  let { loading, data, error } = useFetch(
+    "https://lakspaceblog.onrender.com/api/blogs?populate=*"
+  );
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+  console.log(error);
 
-let {loading, data, error} = useFetch('https://api.render.com/deploy/srv-co5k56mv3ddc7392hmo0?key=l-Z5Rr1P67M/blogs?populate=*')
-if(loading) return <p>Loading...</p>
-if(error) return <p>Error!</p>
-console.log(data)
+  console.log(data);
 
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Homepage blogs={data? data:""} />} ></Route>
-        <Route path="/blog/:id" element={<BlogContent blogs={data? data:""} />} ></Route>
-        
+        <Route path="/" element={<Homepage blogs={data ? data : ""} />}></Route>
+        <Route
+          path="/blog/:id"
+          element={<BlogContent blogs={data ? data : ""} />}
+        ></Route>
       </Routes>
     </div>
   );
