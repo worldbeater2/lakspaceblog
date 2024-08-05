a292da57a869dd3205caa210799625662048fa73
worldbeater2 <realdudization@gmail.com>
2024-08-05 17:56:13 +0000

    fix

--- laks-database/src/api/cat/controllers/cat.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * cat controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::cat.cat');
--- laks-database/src/api/cat/services/cat.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * cat service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::cat.cat');
