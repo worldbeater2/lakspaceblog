016c86c5886cc57a91978e204ad76e26a12a6ed4
worldbeater2 <realdudization@gmail.com>
2024-08-06 20:04:43 +0000

    revert

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -13,9 +13,9 @@ const Blogs = ({ blogs }) => {
                                 <div className="card w-96 relative rounded-md hover:cursor-pointer glass hover:shadow-neworange hover:shadow-sm">
                   <figure className="relative">
                     <img
-                      className="h-[300px] overflow-hidden  bg-cover"
+                      className="h-[300px] overflow-hidden object-cover"
                       src={`${blog.attributes.coverImg.data.attributes.url}`}
-                      alt=""
+                      alt="Paps Beach"
                     />
                     {/* <div className="badge absolute bottom-2 right-2 bg-black bg-opacity-20 text-white p-2">
                       Travel
